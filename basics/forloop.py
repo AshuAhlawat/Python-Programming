6086cee279fb1ed5dba795a333ef65fbaee85de3
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-16 16:11:15 +0530

    main

--- basics/forloop.py
+++ basics/forloop.py
@@ -25,3 +25,13 @@ names=[]
 for name in given_username:
     names.append(name.title().replace("_"," "))
     print(names)
+    
+    #making a word counter in from a name of a book
+name_of_the_book=['the','great','expidition','of','the','great','alexander']
+word_counter={}
+for word in name_of_the_book:
+    if word not in word_counter:
+        word_counter[word]=1
+    else:
+        word_counter[word] += 1
+    print(word_counter)
