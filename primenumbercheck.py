6ce18d52920383bc73bf24a4954bc4a124941fd9
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-19 20:34:45 +0530

    main

--- primenumbercheck.py
+++ primenumbercheck.py
@@ -1,8 +1,9 @@
-check_prime = [26, 39, 51, 53, 57, 79, 85]
+check_prime = [11,13,45,59,66,22,10]
 for number in check_prime:
     for i in range(2, int((number+1)/2)):
         if (number % i) == 0:
             print("{} is NOT a prime number, because {} is a factor of {}".format(number, i, number))
             break
-        elif i==number-1:
+        elif i==(number+1)/2:
             print("{} is a prime number".format(number))
+        
