a13257929fb8d0f0144b7f9c57415619d34339ed
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-19 12:01:10 +0530

    main

--- basics/whileloop.py
+++ basics/whileloop.py
@@ -1,4 +1,4 @@
-to explain while loop we will use an example to use while loop
+#to explain while loop we will use an example to use while loop
 card_deck=[4,11,8,5,13,2,8,10]
 hand=[]
 
@@ -6,7 +6,7 @@ while sum(hand) <= 17 :
     hand.append(card_deck.pop())
 print(hand)
 
-factorial with while loops
+#factorial with while loops
 number=6
 product=1
 current=1
