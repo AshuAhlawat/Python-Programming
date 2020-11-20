e100fa7a6b8e1cba72401668756a3abb7ed8b7a5
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-20 23:05:16 +0530

    mnai

--- basics/listcomprehension.py
+++ basics/listcomprehension.py
@@ -11,3 +11,27 @@ print(captalized_city)
 
 squares = [x**2 if x % 2 == 0 else x+3 for x in range(11)]
 print(squares)
+
+# some practise problems
+
+names = ["Rick Sanchez", "Morty Smith",
+         "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.lower()for name in names]
+print(first_names)
+
+multiples_3=[x*3 for x in range(21)]
+print(multiples_3)
+
+#making a list of passed student
+
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name,score in scores.items() if score>=65]
+print(passed)
