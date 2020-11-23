2b2f6f816c44810ecf09f5a4ee063d93720dbd29
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-23 21:51:19 +0530

    mian

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+        most_win_director.clear()
+        most_win_director.append(key)
+    elif value==highest_number:
+        most_win_director.append(key)
+    else:
+        continue
+
+
+print("Most Oscar Winning Director = {}".format(most_win_director))
