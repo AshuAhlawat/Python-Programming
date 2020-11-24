df541c72bd5f7c38a8a96a73676f8bf68e9df7c8
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-25 03:07:37 +0530

    main

--- basics/RegEx.py
+++ basics/RegEx.py
@@ -1,11 +1,10 @@
 #this is a library to use eith character and metacharacter
 import re
-from typing import Text
 txt = "the rain in spain"
 x = re.search("portugal", txt)
-y = re.search("spain", txt)
+y = re.search("ai", txt)
 z = re.findall("ai", txt)
-m = re.sub("ai", "(lodu lalit)", txt)
+m = re.sub("rain", "lodu lalit", txt)
 print(x)
 print(y)
 print(z)
