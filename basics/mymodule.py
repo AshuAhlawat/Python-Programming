cfdb183d1ae02b698dcc1ff317ef7c37c0482d73
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-11-24 22:04:53 +0530

    mkaj

--- basics/mymodule.py
+++ basics/mymodule.py
@@ -1,9 +1,9 @@
 def greeting(name):
     print("Welcome, "+name)
 
-person1={
-"name":"Ashwani",
-"age":18,
-"country":"India"
-}
 
+person1 = {
+    "name": "Ashwani",
+    "age": 18,
+    "country": "India"
+}
--- basics/mymoduleimport.py
+++ basics/mymoduleimport.py
@@ -1,12 +1,12 @@
+from mymodule import person1
 import mymodule as mx
-a=mx.person1["age"]
-b=mx.person1["name"]
-c=mx.person1["country"]
-d=dir(mx)
+a = mx.person1["age"]
+b = mx.person1["name"]
+c = mx.person1["country"]
+d = dir(mx)
 print(d)
 print(a)
 print(b)
 print(c)
 
-from mymodule import person1
 print(person1["age"])
