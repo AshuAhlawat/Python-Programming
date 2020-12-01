01dc0609cce627f0bff1759169d4eff9c7ea3dc1
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2020-12-01 12:48:24 +0530

    done

--- indiannotation.py
+++ indiannotation.py
@@ -1,12 +1,12 @@
+#constrains= len of the input is must not be greater than 10 digits
 notaion = int(input("Enter the currency here: "))
 input_list = list(str(notaion))
 input_len = len(input_list)
 res = ""
-if input_len <= 3:
-    print(notaion)
-elif input_len >= 4:
-    for i in range(-3, -input_len-1, -3):
-        input_list.insert(i, ",")
+input_list.reverse()
+for i in range(3, (input_len-3)*2, 3):
+    input_list.insert(i, ",")
+input_list.reverse()
 for i in input_list:
     res += i
 print(res)
