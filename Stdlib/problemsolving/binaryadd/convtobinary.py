4743b949166d77f155ff68fd6ee732261f554e83
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2021-09-15 21:37:20 +0530

    Decimal to binary (goint to make a change)

---
+++ Stdlib/problemsolving/binaryadd/convtobinary.py
@@ -0,0 +1,19 @@
+def dec2bin(num):
+    binary=[]
+    rev_binary=[]
+    while num>1:
+        rev_binary.append(int(num%2))
+        num /= 2
+    for i in range(len(rev_binary)-1,-1,-1):
+        binary.append(rev_binary[i])
+    if (allsame(binary)):
+        binary.insert(0,1)
+    print(*binary)
+    
+
+def allsame(binary):    
+    first= binary[0]
+    for i in range(len(binary)):
+        if first != binary[i]:
+            return False
+    return True
